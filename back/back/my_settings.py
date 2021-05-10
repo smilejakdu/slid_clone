ef05d87cf2a41b0883161a3326003eeac793bc2c
smilejakdu <ash982416@gmail.com>
2021-05-10 19:57:57 +0900

    back_setting

---
+++ back/back/my_settings.py
@@ -0,0 +1,25 @@
+DATABASES = {
+    'default' : {
+        'ENGINE'   : 'django.db.backends.mysql',
+        'HOST'     : 'localhost',
+        'NAME'     : 'slid_clone',
+        'USER'     : 'root',
+        'PASSWORD' : '##tkakrnl12',
+        'PORT'     : '3306',
+        'TEST'     :
+            {
+                'CHARSET'   : 'utfmb4',
+                'COLLATION' : 'utf8mb4_general_ci',
+            }
+    }
+}
+
+
+SECRET_KEY = {
+    'secret' :'!1x9$j8w*voo6v#+79rmow$+v=-w!diymd-63ooe_wjj_)4yw7',
+}
+ALGORITHM = 'HS256'
+
+
+
+
