6b28f51a87aece68f5b88aadd940f846babe6a3b
smilejakdu <ash982416@gmail.com>
2021-05-16 18:52:05 +0900

    test_code_signin

--- back/user/tests.py
+++ back/user/tests.py
@@ -58,7 +58,42 @@ class UserTest(TestCase):
             'password' : '12345678',
             'active'   : 1
         }
+
         response = client.post('/users/signup', json.dumps(user), content_type='application/json')
 
         self.assertEqual(response.status_code, 400)
         self.assertEqual(response.json(),{"message": "KEY_ERROR"})
+
+    def test_SigninView_post_success(self):
+        client = Client()
+        sign_in_user = {
+            "email"    : "test@gmail.com",
+            "password" : "12345678"
+        }
+
+        response = client.post('/users/signin', json.dumps(sign_in_user), content_type='application/json')
+        self.assertEqual(response.status_code, 200)
+
+
+    def test_SigninView_post_password_incorret(self):
+        client = Client()
+        sign_in_user = {
+            "email"    : "test@gmail.com",
+            "password" : "incorret"
+        }
+        response = client.post('/users/signin', json.dumps(sign_in_user), content_type='application/json')
+
+        self.assertEqual(response.status_code, 401)
+        self.assertEqual(response.json(),{"message": "INVALID_PASSWORD"})
+
+
+    def test_EmailSigninView_post_user_not_exists(self):
+        client = Client()
+        sign_in_user = {
+            'email'    : 'notexists@gmail.com',
+            'password' : '123456789',
+        }
+        response = client.post('/users/signin', json.dumps(sign_in_user), content_type='application/json')
+
+        self.assertEqual(response.status_code, 401)
+        self.assertEqual(response.json(),{"message": "INVALID_USER"})
