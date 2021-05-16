6061825be46b6fb91709ee29a630c52202fab0eb
smilejakdu <ash982416@gmail.com>
2021-05-17 05:52:39 +0900

    back_product_test_put

--- back/product/tests.py
+++ back/product/tests.py
@@ -9,9 +9,9 @@ class UserTest(TestCase):
 
     def setUp(self):
         Product.objects.create(
-            name        = "Basic",
-            price       = 6700,
-            description = "description test",
+            name        = "Basic_string",
+            price       = 80,
+            description = "description_test_string",
             active      = True
             )
 
@@ -21,12 +21,37 @@ class UserTest(TestCase):
     def test_ProductView_post_success(self):
         client = Client()
         product   = {
-            'name'        : 'Basic',
-            'price'       : 6700,
-            'description' : "description test",
+            'name'        : 'Basic_string',
+            'price'       : 80,
+            'description' : "description_test_string",
             'active'      : True
         }
 
         response = client.post('/product', json.dumps(product), content_type='application/json')
         self.assertEqual(response.status_code,201)
+
+    def test_ProductView_post_key_error(self):
+        client = Client()
+        user = {
+            'name'  : 'Basic',
+            'price' : 6700,
+            'des'   : "description_test_error",
+        }
+
+        response = client.post('/product', json.dumps(user), content_type='application/json')
+
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json(),{"message": "KEY_ERROR"})
+
+    def test_ProductView_put_success(self):
+        client = Client()
+        product   = {
+            'name'        : 'Basic_string',
+            'price'       : 801,
+            'description' : "description_test_string",
+            'active'      : True
+        }
+
+        response = client.put('/product', json.dumps(product), content_type='application/json')
+        self.assertEqual(response.status_code,200)
 
