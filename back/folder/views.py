35657ff21deca31863ab463e046721ac0a1ca973
smilejakdu <ash982416@gmail.com>
2021-05-20 03:50:57 +0900

    slid_folder_get

--- back/folder/views.py
+++ back/folder/views.py
@@ -15,31 +15,23 @@ class FolderView(View):
     @login_check
     def post(self, request):
         data = json.loads(request.body)
-        print(data)
-# {'name': 'folder1', 'depth_idx': 1, 'trash_basket': False, 'parent_folder_id': 0}
-        print("request.body :" , request.user) # test@gmail.com
-        print("request.body :" , request.user.id) # 1 
-        print(data['name'] if data['name'] else 'Untitled folder')
-        print(0 if data['depth_idx'] == 0 else data['depth_idx'])
-        print(data['trash_basket'])
-        print(data['parent_folder_id'])
-        # value Error 발생 .. 잘못된게 없는것같은데 .. ?
+
         try:
             Folder(
                 name             = data['name'] if data['name'] else 'Untitled folder',
-                depth_idx        = 0 if data['depth_idx'] == 0 else data['depth_idx'],
-                trash_basket     = data['trash_basket'],
-                parent_folder_id = 0 if data['parent_folder_id']==0 else data['parent_folder_id'],
+                depth_idx        = 1 if data['depth_idx'] == 1 else data['depth_idx'],
+                trash_basket     = False,
+                parent_folder_id = data['parent_folder_id'],
                 user_id          = request.user.id
             ).save()
 
-            return JsonResponse({"message": "INSERT_FOLDER"},status=201)
+            return JsonResponse({"message": "INSERT_SUCCESS_FOLDER"},status=201)
 
         except KeyError: # 키값 확인
             return JsonResponse({"message": "INVALID_KEY"},status=400)
 
-        # except ValueError: # value 값 확인
-        #     return JsonResponse({"message": "INVALID_VALUE"},status=400) 
+        except ValueError: # value 값 확인
+            return JsonResponse({"message": "VALUE_ERROR"},status=400) 
 
         except Exception as e:
             return JsonResponse({"message": e},status=400)
@@ -64,10 +56,17 @@ class FolderView(View):
                 folder.save()
                 return JsonResponse({"message":"TRASH_BASKET_SUCCESS"} ,status=200)
 
-            elif data['parent_folder'] and data['depth_idx']: # 폴더이동
+            elif data['parent_folder_id'] and data['depth_idx']: # 폴더이동
+                folder.parent_folder_id = data['parent_folder_id']
+                folder.depth_idx        = data['depth_idx']
+                folder.save()
 
                 return JsonResponse({"message":"TRASH_BASKET_SUCCESS"} ,status=200)
-        except KeyError :
+
+        except ValueError :# value 에러
+            return JsonResponse({"message":"VALUE_ERROR"} ,status=400)
+
+        except KeyError : # key 에러 
             return JsonResponse({"message":"KEY_ERROR"} ,status=400)
 
         except Exception as e :
@@ -94,7 +93,14 @@ class FolderView(View):
 
     @login_check
     def get(self ,request):
-        # 기본값 id 순으로 출력 
+        print(request.user.id)
+        # 기본값 depth_idx 순으로 출력  ==> 첫번째 바로 보여주게 될때 
+        # 일반적인 검색이랑 같지않을까?? 생각을 함 
+        folder = (Folder.
+                  objects.
+                  prefetch_related('folder_set').
+                  filter(depth_idx=1))
+
         # 생성일순 으로 출력
         # 이름순으로 출력
         return
