4f858ca320a29df3c9110c83d371a06332575444
smilejakdu <ash982416@gmail.com>
2021-05-21 21:57:44 +0900

    get_root_path_error

--- back/folder/views.py
+++ back/folder/views.py
@@ -31,7 +31,7 @@ class FolderView(View):
             return JsonResponse({"message": "INVALID_KEY"},status=400)
 
         except ValueError: # value 값 확인
-            return JsonResponse({"message": "VALUE_ERROR"},status=400) 
+            return JsonResponse({"message": "VALUE_ERROR"},status=400)
 
         except Exception as e:
             return JsonResponse({"message": e},status=400)
@@ -94,62 +94,64 @@ class FolderView(View):
 
     @login_check
     def get(self ,request):
-        # depth_idx = 1 보여줌 
-        # 조건문을 걸어서 , 
 
         order_filter         = request.GET.get("order",None)
-        data                 = json.loads(request.json)
-        depth_idx_filter     = int(data.get('depth_idx',None))
-        parent_folder_filter = int(data.get('parent_folder_id',None))
-        data_id              = int(data.get(id , None))
+        data                 = json.loads(request.body)
+
+#        depth_idx_filter     = int(data.get('depth_idx',None))
+#        parent_folder_filter = int(data.get('parent_folder_id',None))
+        data_id              = int(data.get('id' , None))
 
         result_data          = []
 
         try:
-            if len(order_filter) > 1: # 이름순 , 생성일 순 order
-                folders = (Folder.
-                           objects.
-                           prefetch_related('folder_set').
-                           filter(depth_idx = depth_idx_filter).order_by(f'{order_filter}'))
+#            if len(order_filter) > 1: # 이름순 , 생성일 순 order
+#                folders = (Folder.
+#                           objects.
+#                           prefetch_related('folder_set').
+#                           filter(depth_idx = depth_idx_filter).order_by(f'{order_filter}'))
+#
+#                folder_data = [{
+#                     'id'               : folder.id,
+#                     'name'             : folder.name,
+#                     'depth_idx'        : folder.depth_idx,
+#                     'trash_basket'     : folder.trash_basket,
+#                     'parent_folder_id' : folder.parent_folder_id,
+#                     'created_at'       : folder.created_at,
+#                 }for folder in folders]
+#                data.append({'current_path_data': folder_data})
+#
+#            else:
+#                folder = (Folder.
+#                          objects.
+#                          prefetch_related('folder_set').
+#                          filter(depth_idx = depth_idx_filter))
+#                folder_data = [{
+#                     'id'               : folder.id,
+#                     'name'             : folder.name,
+#                     'depth_idx'        : folder.depth_idx,
+#                     'trash_basket'     : folder.trash_basket,
+#                     'parent_folder_id' : folder.parent_folder_id,
+#                     'created_at'       : folder.created_at,
+#                 }for folder in folders]
 
-                folder_data = [{
-                     'id'               : folder.id,
-                     'name'             : folder.name,
-                     'depth_idx'        : folder.depth_idx,
-                     'trash_basket'     : folder.trash_basket,
-                     'parent_folder_id' : folder.parent_folder_id,
-                     'created_at'       : folder.created_at,
-                 }for folder in folders]
-                data.append({'current_path_data': folder_data})
-
-            else:
-                folder = (Folder.
-                          objects.
-                          prefetch_related('folder_set').
-                          filter(depth_idx = depth_idx_filter))
-
-                folder_data = [{
-                     'id'               : folder.id,
-                     'name'             : folder.name,
-                     'depth_idx'        : folder.depth_idx,
-                     'trash_basket'     : folder.trash_basket,
-                     'parent_folder_id' : folder.parent_folder_id,
-                     'created_at'       : folder.created_at,
-                 }for folder in folders]
-            # 이부분 재귀로 돌려야 할까 ?? 
             root_path_list_deq = collections.deque([])
             root_path          = (Folder.objects.select_related('parent_folder').
                                   get(id = data_id))
+            print("1 :",root_path) # 1 : Folder object (11)
+            print("2 :",root_path.parent_folder.parent_folder_id) # 2 : 1
 
             while root_path.name: # 재귀 함수 안쓰고 while 문으로는 할수없을까 ??저녁먹고 다시해보자
+                print(root_path.name)
                 root_path_list_deq.appendleft(root_path.parent_folder.name)
-                if root_path.parent_folder_id == '' or root_path.parent_folder_id == None:
+
+                if root_path.parent_folder.parent_folder_id == '' or root_path.parent_folder.parent_folder_id == None:
                     break
                 root_path = (Folder.objects.select_related('parent_folder').
                              get(id = root_path.parent_folder.parent_folder_id))
-
-            return JsonResponse({"data": list(folder)}, status=200)
-
+            print("3")
+            return HttpResponse(status=200)
+#            return JsonResponse({"data": list(folder)}, status=200)
         except TypeError:
             return JsonResponse({"message": "INVALID_TYPE"}, status=400)
 
